e6f698e8a0986f9cef319eb43c8ee8003cd3d41d
bpaser <60895408+bpaser@users.noreply.github.com>
2022-08-11 10:54:15 -0500

    first commit

---
+++ websocket-bk-2/src/main/java/com/example/websocketbk2/WebsocketBk2Application.java
@@ -0,0 +1,13 @@
+package com.example.websocketbk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketBk2Application.class, args);
+	}
+
+}
---
+++ websocket-bk-2/src/test/java/com/example/websocketbk2/WebsocketBk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketbk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketBk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
